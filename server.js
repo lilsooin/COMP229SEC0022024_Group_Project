cd5b9273b32d6ba8de23bbacd11d2fc7c2f6bbb3
sooin <lilesooin@gmail.com>
2024-12-05 13:35:29 -0500

    npm buil

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import config from './config/config.js'
 import app from './server/express.js'
 import mongoose from 'mongoose'
+
 mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri, {
     //useNewUrlParser: true,
